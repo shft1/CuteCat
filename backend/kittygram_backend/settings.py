6deaf9e194461250df4e656009bbd24ec7bbd3dd
alex <alexmal24122004@gmail.com>
2024-03-17 12:53:21 +0300

    review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
